a0da52ddd0ab5b2486573789913661db2eafa7e1
Franco Narvaez <juanfranconarvaez@gmail.com>
2024-11-07 23:11:44 -0300

    Formulario Interactivo

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+// Array de ids de los campos
+const campos = ['nombre', 'apellido', 'email', 'telefono', 'edad', 'direccion', 'provincia', 'codigo_postal'];
+
+// Función para actualizar el contenido de la tabla al cambiar un valor en el formulario
+function actualizarTabla(id) {
+    document.querySelector(`td[data-field="${id}"]`).textContent = document.getElementById(id).value;
+}
+
+// Añadimos un event listener para cada campo que ejecuta la función de actualización
+campos.forEach(id => {
+    document.getElementById(id).addEventListener('input', function() {
+        actualizarTabla(id);
+    });
+});
